8114234ee0849e3adeb8bd9f55a0b16619cd50a0
Pragy Agarwal <agar.pragy@gmail.com>
2019-12-21 19:22:50 +0530

    init

---
+++ src/test/java/app/psych/game/GameApplicationTests.java
@@ -0,0 +1,13 @@
+package app.psych.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
